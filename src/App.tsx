e5badeed7b94437e7d86a7f2c1481410c4496fe3
[dyad] <git@dyad.sh>
2025-09-22 19:18:30 +0500

    Simplified App.tsx to render only Index component to debug React.Children.only error - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import AuthLayout from "./components/layout/AuthLayout";
-import ChatPage from "./pages/ChatPage";
 import React from "react";
-
-// Temporarily commented out Toaster and Sonner imports
-// import { Toaster } from "@/components/ui/toaster";
-// import { Toaster as Sonner } from "@/components/ui/sonner";
-
-// Temporarily removed QueryClient and QueryClientProvider imports
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// const queryClient = new QueryClient();
 
 const App = () => (
   <React.Fragment>
-    {/* Temporarily removed Toaster and Sonner */}
-    {/* <Toaster /> */}
-    {/* <Sonner /> */}
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/login" element={<AuthLayout><LoginPage /></AuthLayout>} />
-        <Route path="/register" element={<AuthLayout><RegisterPage /></AuthLayout>} />
-        <Route path="/chat" element={<ChatPage />} />
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Index />
   </React.Fragment>
 );
 
