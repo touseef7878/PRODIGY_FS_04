1ba5107c648f713c47841c9c9c1a61a0cd4314a2
[dyad] <git@dyad.sh>
2025-09-22 19:20:05 +0500

    Re-added React Router and defined application routes in App.tsx - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import Index from "./pages/Index";
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ChatPage from "./pages/ChatPage";
+import NotFound from "./pages/NotFound";
+import AuthLayout from "./components/layout/AuthLayout";
 
 const App = () => (
-  <React.Fragment>
-    <Index />
-  </React.Fragment>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route
+        path="/login"
+        element={
+          <AuthLayout>
+            <LoginPage />
+          </AuthLayout>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <AuthLayout>
+            <RegisterPage />
+          </AuthLayout>
+        }
+      />
+      <Route path="/chat" element={<ChatPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
