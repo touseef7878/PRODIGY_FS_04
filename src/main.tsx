b24131b9ec53527437e8e66641b2ae975006c856
[dyad] <git@dyad.sh>
2025-09-22 19:14:39 +0500

    Rendered simple div in main.tsx to isolate error - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+// import App from "./App.tsx"; // Temporarily removed
 import "./globals.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <div>Hello from main.tsx!</div>
+);
