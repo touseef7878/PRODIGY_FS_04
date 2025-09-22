3bf31051a55f27d85a4c56d8d153277c72cf8001
[dyad] <git@dyad.sh>
2025-09-22 19:19:08 +0500

    Simplified main.tsx to render a plain div to diagnose persistent React.Children.only error - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "./globals.css";
 
 createRoot(document.getElementById("root")!).render(
-  <App />
+  <div>Hello from main.tsx!</div>
 );
